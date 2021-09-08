cf8844aadec69998d51a0b7e664a2d3fda4bd77e
carlos arce (laptop) <carlos.3101a@gmail.com>
2021-09-08 12:19:42 -0400

    adding css and pre-compiler configuration webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
-const htmlWebpackPlugin=require('html-webpack-plugin');
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
@@ -18,13 +20,18 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css|.styl$/i,
+        use: [miniCssExtractPlugin.loader, "css-loader","stylus-loader"],
+      },
     ],
   },
-  plugins:[
+  plugins: [
     new htmlWebpackPlugin({
-        inject:true,
-        template:'./public/index.html',
-        filename:'./index.html'
-    })
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new miniCssExtractPlugin(),
   ],
 };
