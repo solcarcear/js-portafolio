a65793e73ff2c4f7771589f2a50e3059af6387ce
Carlos Arce <carlos.3101a@gmail.com>
2021-09-17 17:33:40 -0400

    adding and install loaders on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 const miniCssExtractPlugin = require("mini-css-extract-plugin");
-const copyPlugin =require('copy-webpack-plugin');
+const copyPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    assetModuleFilename:'assets/images/[hash][ext][query]'
   },
   resolve: {
     extensions: [".js"],
@@ -22,11 +23,25 @@ module.exports = {
       },
       {
         test: /\.css|.styl$/i,
-        use: [miniCssExtractPlugin.loader, "css-loader","stylus-loader"],
+        use: [miniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
       },
       {
-        test:/\.png/,
+        test: /\.png/,
         type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false
+          }
+        }
       }
     ],
   },
@@ -38,8 +53,8 @@ module.exports = {
     }),
     new miniCssExtractPlugin(),
     new copyPlugin({
-      patterns:[{
-        from: path.resolve(__dirname,"src","assets/images"),
+      patterns: [{
+        from: path.resolve(__dirname, "src", "assets/images"),
         to: "assets/images"
       }]
     })
