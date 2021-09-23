05b8d78523c480b950abb51fc6798494f6feef78
carlos arce (laptop) <carlos.3101a@gmail.com>
2021-09-23 10:50:45 -0400

    Adding optimizer plugin and terser and fixing the references with copy-webpack package

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
 const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 const miniCssExtractPlugin = require("mini-css-extract-plugin");
-const copyPlugin = require('copy-webpack-plugin');
+const copyPlugin = require("copy-webpack-plugin");
+const cssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const terserPlugin = require("terser-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-    assetModuleFilename:'assets/images/[hash][ext][query]'
+    filename: "[name].[contenthash].js",
+    assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   resolve: {
     extensions: [".js"],
+    alias: {
+      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@templates": path.resolve(__dirname, "src/templates/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@images": path.resolve(__dirname, "src/assets/images/"),
+    },
   },
   module: {
     rules: [
@@ -27,22 +35,22 @@ module.exports = {
       },
       {
         test: /\.png/,
-        type: 'asset/resource'
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2)$/,
         use: {
-          loader: 'url-loader',
+          loader: "url-loader",
           options: {
             limit: 10000,
             mimetype: "application/font-woff",
-            name: "[name].[ext]",
+            name: "[name]'[contenthash].[ext]",
             outputPath: "./assets/fonts/",
             publicPath: "./assets/fonts/",
-            esModule: false
-          }
-        }
-      }
+            esModule: false,
+          },
+        },
+      },
     ],
   },
   plugins: [
@@ -51,12 +59,22 @@ module.exports = {
       template: "./public/index.html",
       filename: "./index.html",
     }),
-    new miniCssExtractPlugin(),
+    new miniCssExtractPlugin({
+      filename:'assets/[name].[contenthash].css'
+    }),
     new copyPlugin({
-      patterns: [{
-        from: path.resolve(__dirname, "src", "assets/images"),
-        to: "assets/images"
-      }]
-    })
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        },
+      ],
+    }),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new cssMinimizerPlugin(),
+      new terserPlugin()],
+  },
 };
